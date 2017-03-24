cd768fc47917331b26ce7521b448f236aba0b9e1
Laurens Rietveld <laurens.rietveld@gmail.com>
2017-03-24 16:53:38 +0100

    added support for fetching url string from short-string

--- src/shortenerService.js
+++ src/shortenerService.js
@@ -19,6 +19,21 @@ module.exports = function(app) {
 				return res.send(config.server.shortUrlBasename + result.short);
 			})
 		})
+		app.get('/short/:short/url',
+		function (req, res, next) {
+			res.setHeader('Access-Control-Allow-Headers', 'accept, authorization, content-type, x-requested-with');
+			res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+			res.setHeader('Access-Control-Allow-Origin', req.header('origin') || '*');
+			next();
+		},
+		function(req, res) {
+			shortener.fetch(req.params.short, function(err, result) {
+				if (err) return res.status(500).send(err.err);
+				if (!result) return res.status(404).send('No URL found for ' + req.protocol + '://' + req.get('host') + req.originalUrl);
+				res.send(result.url);
+			})
+		}
+	)
 		app.use('/short/:short', function(req, res) {
 			shortener.fetch(req.params.short, function(err, result) {
 				if (err) return res.status(500).send(err.err);
